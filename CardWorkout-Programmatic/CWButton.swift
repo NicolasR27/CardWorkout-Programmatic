cd25c76e14a15fa77b0090a1e6d1d6f773eb9561
Nicolas Rios <nickrios33@yahoo.com>
2020-04-06 16:56:34 -0700

    added custom cw button

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,30 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Nicolas Rios on 4/6/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(){
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize:19,weight:.bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
